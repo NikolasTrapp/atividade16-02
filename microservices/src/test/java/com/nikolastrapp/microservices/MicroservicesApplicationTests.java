cd7a1dc56e9ad52b7e66180ba64a87237c241127
NikolasTrapp <nikolastrapp@gmail.com>
2023-01-24 14:48:38 -0300

    Reunindo todos os projetos

---
+++ microservices/src/test/java/com/nikolastrapp/microservices/MicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikolastrapp.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- src/main/java/com/nikolastrapp/projeto/repositories/ParkingSpotRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nikolastrapp.projeto.repositories;
-
-import com.nikolastrapp.projeto.entities.ParkingSpot;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ParkingSpotRepository extends JpaRepository<ParkingSpot, Long> {
-}
