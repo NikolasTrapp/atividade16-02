9d8d53e456ea4a1601b51cfb08d5652ee72f2a2b
NikolasTrapp <nikolastrapp@gmail.com>
2023-02-01 15:48:12 -0300

    Concluindo v2 do desafio

--- desafio/src/main/java/com/nikolastrapp/desafio/dtos/MutanteDto.java
+++ desafio/src/main/java/com/nikolastrapp/desafio/dtos/MutanteDto.java
@@ -1,6 +1,9 @@
 package com.nikolastrapp.desafio.dtos;
 
+import com.nikolastrapp.desafio.models.enums.Especie;
+import com.nikolastrapp.desafio.models.enums.NivelPoder;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
@@ -8,5 +11,15 @@ public class MutanteDto {
 
     @NotBlank
     private String name;
+    @NotBlank
+    private Especie especie;
+    @NotNull
+    private Integer idade;
+    @NotBlank
+    private String poder;
+    @NotBlank
+    private NivelPoder nivelPoder;
+    @NotBlank
+    private String email;
 
 }
